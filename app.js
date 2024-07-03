07cbbb49506c114c83e5e097f86da281bd935ed8
hallydon <hallydon006@gmail.com>
2024-07-03 23:41:56 +0100

    switch to ipapi

--- app.js
+++ app.js
@@ -10,13 +10,18 @@ app.get('/api/hello', async (req, res) => {
     let location = 'Unknown';
     let temperature = 'Unknown';
     try {
-    
-        const locationResponse = await axios.get(`http://ipinfo.io/${clientIp}/json?token=e05d8cafc359e3`);
+        const locationResponse = await axios.get(`https://ipapi.co/${clientIp}/json/`);
         location = locationResponse.data.city || 'Unknown';
+    
+        // const locationResponse = await axios.get(`http://ipinfo.io/${clientIp}/json?token=e05d8cafc359e3`);
+        // location = locationResponse.data.city || 'Unknown';
 
         if (location !== 'Unknown') {
-            const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=c581371dfbe5dc70c0fa3e48da2ff3be`);
-            temperature = weatherResponse.data.main.temp || 'Unknown';
+            const apiKey = 'c581371dfbe5dc70c0fa3e48da2ff3be';
+            const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`);
+            temperature = weatherResponse.data.current.temp_c || 'Unknown';
+            // const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=c581371dfbe5dc70c0fa3e48da2ff3be`);
+            // temperature = weatherResponse.data.main.temp || 'Unknown';
         }
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -30,7 +35,7 @@ app.get('/api/hello', async (req, res) => {
     });
 });
 
-const port = 3000;
+const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`);
 });
