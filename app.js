5a9125fe902ec7c7858ef4a3c85ff5db1eb7592e
hallydon <hallydon006@gmail.com>
2024-07-04 16:20:34 +0100

    fetching client Ip

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const axios = require('axios');
 
 app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || 'Guest';
+    
     const clientIp = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    console.log(clientIp);
     
     
     let location = 'Unknown';
@@ -12,16 +14,17 @@ app.get('/api/hello', async (req, res) => {
     try {
         const locationResponse = await axios.get(`https://ipapi.co/${clientIp}/json/`);
         location = locationResponse.data.city || 'Unknown';
+        console.log(location);
     
-        // const locationResponse = await axios.get(`http://ipinfo.io/${clientIp}/json?token=e05d8cafc359e3`);
-        // location = locationResponse.data.city || 'Unknown';
+       
 
         if (location !== 'Unknown') {
             const apiKey = process.env.WEATHER_API_KEY || 'c581371dfbe5dc70c0fa3e48da2ff3be';
-            const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`);
-            temperature = weatherResponse.data.current.temp_c || 'Unknown';
-            // const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=c581371dfbe5dc70c0fa3e48da2ff3be`);
-            // temperature = weatherResponse.data.main.temp || 'Unknown';
+            const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`);
+            console.log(weatherResponse);
+            temperature = weatherResponse.data.main.temp || 'Unknown';
+            
+        
         }
     } catch (error) {
         console.error('Error fetching data:', error);
